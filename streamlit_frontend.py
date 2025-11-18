265e607aa43839fb9cbb1aad9c9912ad577c31c0
tushargandhi77 <gandhitushar418@gmail.com>
2025-11-18 20:54:48 +0530

    Implement chatbot functionality with Streamlit frontend and integrate message handling

---
+++ streamlit_frontend.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+
+CONFIG = {'configurable':{'thread_id':'1'}}
+# SEssion state is not erased 
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input("type your message here...")
+
+if user_input:
+    
+    # first add the message to message history
+    st.session_state['message_history'].append({'role':'user', 'content': user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+    
+    
+    response = chatbot.invoke({'messages':[HumanMessage(content=user_input)]},config = CONFIG)
+    ai_message = response['messages'][-1].content
+    st.session_state['message_history'].append({'role':'assistant', 'content': ai_message})
+    with st.chat_message("assistant"):
+        st.text(ai_message)
